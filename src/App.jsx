312b75750cb57c727fcd59b83eeebb4a5e9fda70
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-09-30 02:12:07 +0530

    adding the trending route

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./views/home";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-import './assets/styles/main.scss'
+import "./assets/styles/main.scss";
 import ProductCategory from "./views/productCategories";
+import Trending from "./views/trending";
 function App() {
-  
   return (
-    <div className="w-full flex flex-col overflow-hidden" style={{scrollbarWidth:'none'}}>
+    <div
+      className="w-full flex flex-col overflow-hidden"
+      style={{ scrollbarWidth: "none" }}
+    >
       <Navbar />
       <div className="w-full flex justify-center">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product-category" element={<ProductCategory />} />
+          <Route path="/trending" element={<Trending />} />
         </Routes>
       </div>
       <Footer />
